aebae4ae0901b2b47b78a9fbf160c842f5c7f5ee
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-03-31 23:13:16 +0200

    added winrm installation

--- ansible/install.sh
+++ ansible/install.sh
@@ -5,3 +5,7 @@ ANSIBLE_GIT_REPO=git://github.com/dresden-weekly/ansible.git
 ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.0.1-clean}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
+
+# install winrm
+sudo apt-get install python-pip
+sudo pip install pywinrm
