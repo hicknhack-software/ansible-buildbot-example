994658c6bb1a63881d81990dc9a0c944359c0b86
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-04-12 22:47:07 +0200

    split the mingw task and simplified it a bit. * upgraded to patched ansible

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,7 +2,7 @@
 
 #   Version of Ansible that is used
 ANSIBLE_GIT_REPO=git://github.com/dresden-weekly/ansible.git
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.0.1-clean}
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.0.1-clean-p1}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
 
