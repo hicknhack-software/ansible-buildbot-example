c8c71dcf960b6c8f58c9124a407b8a3b95b1e041
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-06-24 17:01:43 +0200

    upgraded Ansible version

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,7 +2,7 @@
 
 #   Version of Ansible that is used
 ANSIBLE_GIT_REPO=git://github.com/dresden-weekly/ansible.git
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.0.1-clean-p1}
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.1-clean}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
 
