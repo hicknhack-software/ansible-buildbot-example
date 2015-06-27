610692bad1650538fb99e3d9fa7e763328585964
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-06-27 12:20:13 +0200

    upgraded to cleaned ansible 1.9.2

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,9 +2,10 @@
 
 #   Version of Ansible that is used
 ANSIBLE_GIT_REPO=git://github.com/dresden-weekly/ansible.git
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.1-clean}
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.2-clean}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
 
+#   WinRM support
 sudo apt-get install python-pip -y
 sudo pip install pywinrm
