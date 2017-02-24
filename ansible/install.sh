9cc2cc064ec320be4132cbab4a8af132b08ca4c0
michael starke <michael.starke@hicknhack-software.com>
2017-02-24 16:35:37 +0100

    install-nine is now called install

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #   Version of Ansible that is used
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=2.1.1.0 1}
-ANSIBLE_GIT_BRANCH=v2.1.1.0-1
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=2.2.1.0 1}
+ANSIBLE_GIT_BRANCH=v2.2.1.0-1
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
 
--- ansible/run.sh
+++ ansible/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#ANSIBLE_OPTIONS=-vvvv
+#export ANSIBLE_OPTIONS=-vvvv
 export ANSIBLE_HOST_KEY_CHECKING=false
 
 source "$PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/run.sh"
